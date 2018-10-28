94d72bc8e48d4d48d1710f02c3bf7782759a22ef
santiagoblas <santiagob.beitia@gmail.com>
2018-10-28 20:06:30 -0300

    Curso desde cero primer modulo propio (exports, module.exports)

---
+++ CursoDesdeCero/PrimerModuloPropio/index.js
@@ -0,0 +1,8 @@
+const math = require("./matematica.js");
+
+console.log(math);
+
+// console.log(math.add(1,34));
+// console.log(math.substract(1,34));
+// console.log(math.multiply(1,34));
+// console.log(math.divide(1,34));
