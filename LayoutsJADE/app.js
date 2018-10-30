525ad44aa8e10b3b5d447535630b1687b34ffa72
santiagoblas <santiagob.beitia@gmail.com>
2018-10-30 11:54:04 -0300

    parser con body-parser y app.post para agarrar los datos del login

--- LayoutsJADE/app.js
+++ LayoutsJADE/app.js
@@ -1,11 +1,14 @@
 
 var express = require("express");
-
+var bodyParser = require("body-parser");
 //nos retorna el objeto sobre el cual trabajaremos
 var app = express();
 
 //Doy acceso a mi carpeta public añadiendo el prefijo /estatico a la url
-app.use("/estatico", express.static("public"))
+app.use("/estatico", express.static("public"));
+
+app.use(bodyParser.json()); //para peticiones application/json
+app.use(bodyParser.urlencoded({extended: true /*define con que algoritmo va a hacer el parser la librería. Permite parsear arrays y bla bla*/}));
 
 app.set('view engine', 'jade');
 
@@ -16,5 +19,11 @@ app.get("/", function (req, res) {
 app.get("/login", function (req, res) {
     res.render("login");
 })
+
+app.post("/users", function(req,res) {
+    console.log("Email: " + req.body.email);
+    console.log("Contraseña: " + req.body.password);
+    res.send("Recibimos tus datos");
+});
 
 app.listen(8080);
