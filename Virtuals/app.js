9507d335d9b63da9f0c6ab41d507b0b3340cdd62
Santiago Blas Beitia <santiagob.beitia@gmail.com>
2018-11-03 15:10:50 -0300

    Validaciones básicas de mongoose y prueba en el login/registro

--- Virtuals/app.js
+++ Virtuals/app.js
@@ -33,7 +33,10 @@ app.post("/users", function(req,res) {
 
     console.log(user.password_confirmation);
 
-    user.save(function() {
+    user.save(function(err) {
+        if(err){
+            console.log(String(err));
+        }
         res.send("Guardamos tus datos");
     });
 });
--- Virtuals/models/user.js
+++ Virtuals/models/user.js
@@ -15,14 +15,33 @@ Objectid
 Array
 */
 
+var posibles_valores = ["M", "F", "X"]
+
+var email_match = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Coloca un email válido"]
+
 //Inicializo un schema con esa estructura JSON
 var user_schema = new Schema({
+    /**VALIDACION MONGOOSE
+     * VALID PARA Number
+     * min, max, 
+     *VALID PARA String
+     * maxlength, minlength, match, enum
+     * las validaciones de mongoose se hacen sobre el schema.
+     * Las validaciones no required no se aplican sobre type undefined.
+     */
     name: String,
-    username: String,
-    password: String,
-    age: Number,
-    email: String,
-    date_of_birth: Date
+    username: {type: String, required:true,
+            maxlength:[50, "Username muy largo (max: 50)"]},
+    password: {type: String, required:true,
+            minlength:[8, "Pass muy corto"]},
+    age:    {type: Number, min:[5,"La edad no puede ser menor a 5"],
+            max:[100,"La edad máxima es 100"]},
+    email: {type: String, 
+            /**required:true o */ 
+            required: "El email es obligatorio",
+            match: email_match},
+    date_of_birth: Date,
+    sex: {type:String,enum:{values: posibles_valores, message: "Opcion no válida"}}
 });
 
 user_schema.virtual("password_confirmation")
