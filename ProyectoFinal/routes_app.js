9f8874b5c6e1b5c9be327c078abea1be047f7106
Santiago Blas Beitia <santiagob.beitia@gmail.com>
2018-11-12 16:30:59 -0300

    Refactor. Encontrar registro de Imagen en un middleware y reutilizar código.

--- ProyectoFinal/routes_app.js
+++ ProyectoFinal/routes_app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var Imagen = require("./models/imagenes");
 var router = express.Router();
+var image_finder_middleware = require("./middlewares/findImage");
 
 
 router.get("/", function(req,res) {
@@ -9,33 +10,29 @@ router.get("/", function(req,res) {
 
 /**REST */
 
+router.all("/imagenes/:id*", image_finder_middleware);
+
 router.get("/imagenes/new",function(req,res) {
     res.render("app/imagenes/new");
 });
 
 router.get("/imagenes/:id/edit",function(req,res) {
-    Imagen.findById(req.params.id, function(err,imagen) {
-        res.render("app/imagenes/edit", {imagen:imagen});
-    });
+    res.render("app/imagenes/edit");
 });
 
 //Seteamos una url y concatenamos las funciones que identifican las acciones que se pueden realizar sobre el recurso.
 router.route("/imagenes/:id")
     .get(function(req, res) {
-        Imagen.findById(req.params.id, function(err,imagen) {
-            res.render("app/imagenes/show", {imagen:imagen});
-        });
+        res.render("app/imagenes/show");
     })
     .put(function(req, res) {
-        Imagen.findById(req.params.id, function(err,imagen) {
-            imagen.title = req.body.titulo;
-            imagen.save(function(err) {
-                if(!err) {
-                    res.redirect("/app/imagenes");
-                } else {
-                    res.render("app/imagenes/edit",{imagen:imagen});
-                }
-            });
+        res.locals.imagen.title = req.body.titulo;
+        res.locals.imagen.save(function(err) {
+            if(!err) {
+                res.redirect("/app/imagenes");
+            } else {
+                res.render("app/imagenes/edit");
+            }
         });
     })
     .delete(function(req, res) {
